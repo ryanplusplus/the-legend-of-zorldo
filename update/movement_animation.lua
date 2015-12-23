e240a90b7012e11acf84fab386f4ac5263ee6bdf
Ryan Hartlage <ryanplusplus@gmail.com>
2015-12-23 10:38:42 -0500

    Infrastructure for multi-directional movement animation

--- update/movement_animation.lua
+++ update/movement_animation.lua
@@ -9,25 +9,27 @@ end
 return function(scene, dt)
   for entity in pairs(scene:entities_with('animation', 'movement_animations', 'velocity', 'on_ground', 'direction')) do
     if entity.on_ground then
-      if entity.velocity.x == 0 then
-        if entity.direction == 'right' then
-          set_animation(entity, 'idle_right')
-        else
-          set_animation(entity, 'idle_left')
+      if entity.velocity.x ~= 0 or entity.velocity.y ~= 0 then
+        local animation = 'walk'
+
+        if entity.velocity.y > 0 then
+          animation = animation .. '_down'
+        elseif entity.velocity.y < 0 then
+          animation = animation .. '_up'
         end
+
+        if entity.velocity.x > 0 then
+          animation = animation .. '_right'
+        elseif entity.velocity.x < 0 then
+          animation = animation .. '_left'
+        end
+
+        set_animation(entity, animation)
       else
-        if entity.direction == 'right' then
-          set_animation(entity, 'walk_right')
-        else
-          set_animation(entity, 'walk_left')
-        end
+        set_animation(entity, 'idle_' .. entity.direction)
       end
     else
-      if entity.direction == 'right' then
-        set_animation(entity, 'air_right')
-      else
-        set_animation(entity, 'air_left')
-      end
+      set_animation(entity, 'air_' .. entity.direction)
     end
   end
 end
