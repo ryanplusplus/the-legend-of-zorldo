2cc87e2641d0862a9eb586ab7dca895287aa8156
Ryan Hartlage <ryanplusplus@gmail.com>
2015-12-24 09:48:44 -0500

    Simplified collision handling

--- update/SimpleCollision.lua
+++ update/SimpleCollision.lua
@@ -2,18 +2,18 @@ return function(world)
   return function(scene, dt)
     for entity in pairs(scene:entities_with('velocity', 'position', 'size', 'simple_collision')) do
       local collisions
-      local resolved_x, resolved_y
-
       local dx = entity.velocity.x * dt
       local dy = entity.velocity.y * dt
 
       local target_x = entity.position.x + dx
       local target_y = entity.position.y + dy
 
-      _, _, collisions = world:check(entity, target_x, target_y, function() return 'cross' end)
-
-      resolved_x = target_x
-      resolved_y = target_y
+      entity.position.x, entity.position.y, collisions = world:move(
+        entity,
+        target_x,
+        target_y,
+        function() return 'touch' end
+      )
 
       for _, collision in pairs(collisions) do
         scene:new_entity({
@@ -23,15 +23,7 @@ return function(world)
             other = collision.other
           }
         })
-
-        resolved_y = collision.touch.y
-        resolved_x = collision.touch.x
       end
-
-      entity.position.x = resolved_x
-      entity.position.y = resolved_y
-
-      world:update(entity, entity.position.x, entity.position.y)
     end
   end
 end
